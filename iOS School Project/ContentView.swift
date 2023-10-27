d6c5bc4c773238bec31f0b6e351a90166fa7f4b2
Lorenz De Bie <lorenz.debie@student.hogent.be>
2023-10-27 10:46:58 +0200

    Initial Commit

---
+++ iOS School Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS School Project
+//
+//  Created by Lorenz De Bie on 27/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS School Project/iOS_School_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_School_ProjectApp.swift
+//  iOS School Project
+//
+//  Created by Lorenz De Bie on 27/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct iOS_School_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

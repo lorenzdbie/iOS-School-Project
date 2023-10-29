136014dc208195c3999b21195e67505769bfcf42
Lorenz De Bie <lorenz.debie@student.hogent.be>
2023-10-29 18:01:17 +0100

    update deleting redundent code

--- iOS School Project/data/LocationDataProvider.swift
+++ iOS School Project/data/LocationDataProvider.swift
@@ -8,9 +8,6 @@
 import Foundation
 
 struct LocationDataProvider{
-//    let countries:[Country]
-//    let states: [CityState]
-//    let cities: [City]
     
     var countries = ["Belgium":
                         ["Flanders":
@@ -37,21 +34,4 @@ struct LocationDataProvider{
     func getCities(country: String, state: String) -> [String]? {
         return countries[country]?[state]?.sorted()
     }
-    
-//    init(){
-//        countries = [Country("Belgium"), Country("USA")]
-//        states = [CityState("Flanders", country: countries[0]),CityState("Brussels", country: countries[0]), CityState("New York", country: countries[1]), CityState("California", country: countries[1])]
-//        cities = [City("Gent", state: states[0]), City("New York City", state: states[1])]
-//    }
-//    func getCountries() -> [Country]{
-//        return countries
-//    }
-//    
-//    func getStates(countryIndex: Int) -> [CityState]{
-//        return states.filter{ $0.country == countries[countryIndex]}
-//    }
-//  
-//    func getCities(stateIndex: Int) -> [City]{
-//        return cities.filter{$0.state == states[stateIndex]}
-//    }
 }
